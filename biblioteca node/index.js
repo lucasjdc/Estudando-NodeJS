783e4a0a7d97c6db139468cc26cd12809c39a08d
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-04-02 15:03:56 -0300

    async/await

---
+++ biblioteca node/index.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro) {
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro)
+    }    
+}
+/*
+function pegaArquivo(caminhoDoArquivo) {
+    // Função assícrona para ler arquivo de texto
+    const encoding = 'utf-8';
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch(trataErro)
+}
+*/
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
