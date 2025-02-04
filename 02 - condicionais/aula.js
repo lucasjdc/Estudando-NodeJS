1c6ea42b4ff25b3dcce4054eef44f62d593edded
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2025-02-04 18:51:28 -0300

    Condicional com Else If

--- 02 - condicionais/aula.js
+++ 02 - condicionais/aula.js
@@ -1,9 +1,10 @@
-const numero = 15;
+const numero = 2;
 const numeroDivisivelPor5 = (numero % 5) === 0;
 
-
-if (numeroDivisivelPor5) {
-	console.log('Sim');
-} else {
+if (numero === 0) {
+	console.log('O número é inválido');
+	} else if (numeroDivisivelPor5) {
+		console.log('Sim');
+	} else {
 	console.log('Não');
 }
